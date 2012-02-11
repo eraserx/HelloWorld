b9b323bcdd851fd7817c0051056b4d7d91c82178
vagrant <vagrant@vagrant.(none)>
2012-02-10 19:37:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '7da2218a64af6db13a09c9e51f4f4fb5b008fef72330a9dcf2df038288b39f55f48822b92cc4e6a9470d247cf104c6411546018fa0fc695dee776f83a32aa5f0'

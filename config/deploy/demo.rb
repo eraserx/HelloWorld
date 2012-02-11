1670a48329a2e6fa2d1366e37d5cc50584d5854d
vagrant <vagrant@vagrant.(none)>
2012-02-10 20:08:32 -0500

    capify the project

---
+++ config/deploy/demo.rb
@@ -0,0 +1,17 @@
+set :application, "rails321"
+set :repository,  "git@github.com:eraserx/HelloWorld.git"
+set :deploy_to, "/var/www/#{application}"
+set :rails_env, "demo"
+set :user, "vagrant"
+set :group, "vagrant"
+set :use_sudo, false
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm_command, "/usr/bin/git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+role :web, "rails321"                   # Your HTTP server, Apache/etc
+role :app, "rails321"                   # This may be the same as your `Web` server
+role :db,  "rails321", :primary => true # This is where Rails migrations will run
